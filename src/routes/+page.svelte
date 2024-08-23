2d3a4944b3a3f81d35da4f8d43f541078c39d7a9
Jack Malcom <j@ckmal.com>
2024-08-22 22:26:49 -0500

    init + /climb redirect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div class="content">
+	<h1>🌭.land</h1>
+	<ul>
+		<a href="/climb">
+			<li>Climb Talks RSVP Form</li>
+		</a>
+	</ul>
+</div>
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
