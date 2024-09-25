8e8bfe59c5b9ec2615bd8a84209c47810de4fa8c
Jack Malcom <j@ckmal.com>
2024-09-25 17:41:16 -0500

    climb

--- src/routes/climb/+page.svelte
+++ src/routes/climb/+page.svelte
@@ -1,9 +1,9 @@
 <div class="content">
-	<h1>🌭.land</h1>
-	<ul>
-		<li><a href="/climb">Climb Talks</a></li>
-		<li><a href="/discord">Error 479 Discord</a></li>
-	</ul>
+	<h1>Climb Talks</h1>
+    <h2>Next event: Tuesday, October 15 @ 7:00 PM</h2>
+    <a href="https://forms.gle/zBb5Dpasb5DVXWsS8">
+        <h2>SIGN UP HERE</h2>
+    </a>
 </div>
 
 <style>
