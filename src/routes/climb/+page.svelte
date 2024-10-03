bf3b38b685561737f57167a9c3c198d4042e087a
Jack Malcom <j@ckmal.com>
2024-10-03 18:34:54 -0500

    add pics, remove content wrapper

--- src/routes/climb/+page.svelte
+++ src/routes/climb/+page.svelte
@@ -1,56 +1,59 @@
-<div class="content">
-	<h1>Climb Talks</h1>
-	<a href="https://forms.gle/zBb5Dpasb5DVXWsS8">
-		<h2 class="button">SIGN UP HERE</h2>
-	</a>
+<script>
+	import sep1 from '$lib/climb/september241.jpg';
+	import sep2 from '$lib/climb/september242.jpg';
+</script>
 
-	<h2>Where:</h2>
-	<p>
-		Boulders & Brews - <a href="https://maps.app.goo.gl/dPfXVcnizaarqAgC8">Maps</a> -
-		<a href="https://bouldersandbrews.com/">Website</a>
-	</p>
+<h1>Climb Talks</h1>
+<a href="https://forms.gle/zBb5Dpasb5DVXWsS8">
+	<h2 class="button">SIGN UP HERE</h2>
+</a>
 
-	<h2>When:</h2>
-	<p>Tuesday, October 15 @ 7:00 PM</p>
+<h2>Where:</h2>
+<p>
+	Boulders & Brews - <a href="https://maps.app.goo.gl/dPfXVcnizaarqAgC8">Maps</a> -
+	<a href="https://bouldersandbrews.com/">Website</a>
+</p>
 
-	<h2>What is Climb Talks?</h2>
-	<p>
-		Climb Talks is a monthly event where people in the software and engineering communities listen
-		to two lightning talks, and then climb and socialize. It's a great way to meet other people in
-		the industry, learn something new, and get some exercise. People of all skill levels are
-		welcome!
-	</p>
+<h2>When:</h2>
+<p>Tuesday, October 15 @ 7:00 PM</p>
 
-	<p>Thanks to Walmart for sponsoring the event!</p>
+<h2>What is Climb Talks?</h2>
+<p>
+	Climb Talks is a monthly event where people in the software and engineering communities listen to
+	two lightning talks, and then climb and socialize. It's a great way to meet other people in the
+	industry, learn something new, and get some exercise. People of all skill levels are welcome!
+</p>
 
-	<h2>What to expect:</h2>
-	<ul>
-		<li>Two 15-minute talks</li>
-		<li>Free pizza</li>
-		<li>Free indoor climbing, climbing shoes included</li>
-	</ul>
+<p>Thanks to Walmart for sponsoring the event!</p>
 
-	<h2>Who is Speaking?</h2>
-	<ul>
-		<li>Evan Black</li>
-		<li>Noah Zimmerman</li>
-	</ul>
+<h2>What to expect:</h2>
+<ul>
+	<li>Two 15-minute talks</li>
+	<li>Free pizza</li>
+	<li>Free indoor climbing, climbing shoes included</li>
+</ul>
 
-	<h2>FAQ:</h2>
-	<h3>Will there be food?</h3>
-	<p>Yes, there will be free pizza and drinks.</p>
+<h2>Who is Speaking?</h2>
+<ul>
+	<li>Evan Black</li>
+	<li>Noah Zimmerman</li>
+</ul>
 
-	<h3>Do I need to climb?</h3>
-	<p>Nope! There is no pressure to climb.</p>
+<h2>FAQ:</h2>
+<h3>Will there be food?</h3>
+<p>Yes, there will be free pizza and drinks.</p>
 
-	<h3>What should I wear to climb?</h3>
-	<p>
-		A t-shirt and sport shorts/sweatpants are best. We will provide rental shoes free of charge.
-	</p>
+<h3>Do I need to climb?</h3>
+<p>Nope! There is no pressure to climb.</p>
 
-	<h3>Do I need to sign a waiver?</h3>
-	<p>Yes, you can do it in person, or online <a href="https://bouldersandbrews.com/">here</a>.</p>
-</div>
+<h3>What should I wear to climb?</h3>
+<p>A t-shirt and sport shorts/sweatpants are best. We will provide rental shoes free of charge.</p>
+
+<h3>Do I need to sign a waiver?</h3>
+<p>Yes, you can do it in person, or online <a href="https://bouldersandbrews.com/">here</a>.</p>
+
+<img src={sep1} width="40%" />
+<img src={sep2} width="40%" />
 
 <style>
 	.button {
