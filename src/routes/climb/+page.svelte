2c7a66a0878434bec518c266f62a2d1985c8e580
Jack Malcom <j@ckmal.com>
2025-01-05 17:36:13 -0600

    add april and november pictures, remove outdated info

--- src/routes/climb/+page.svelte
+++ src/routes/climb/+page.svelte
@@ -1,13 +1,19 @@
 <script>
+	import apr1 from '$lib/climb/april231.jpg';
+	import apr2 from '$lib/climb/april232.jpg';
+
 	import sep1 from '$lib/climb/september241.jpg';
 	import sep2 from '$lib/climb/september242.jpg';
 
 	import oct1 from '$lib/climb/october241.jpg';
 	import oct2 from '$lib/climb/october242.jpg';
+
+	import nov1 from '$lib/climb/november241.jpg';
+	import nov2 from '$lib/climb/november242.jpg';
 </script>
 
 <h1>Climb Talks 🧗</h1>
-<a href="/climb/signup">
+<!-- <a href="/climb/signup">
 	<h2 class="button">SIGN UP HERE</h2>
 </a>
 
@@ -18,16 +24,21 @@
 </p>
 
 <h2>When:</h2>
-<p>Tuesday, November 12 @ 7:00 PM</p>
+<p>Tuesday, November 12 @ 7:00 PM</p> -->
 
-<h2>What is Climb Talks?</h2>
+<h2>What are Climb Talks?</h2>
 <p>
-	Climb Talks is a monthly event where people in the software and engineering communities listen to
+	Climb Talks are a monthly event where people in the software and engineering communities listen to
 	two lightning talks, and then climb and socialize. It's a great way to meet other people in the
 	industry, learn something new, and get some exercise. People of all skill levels are welcome!
 </p>
 
-<p>Thanks to Walmart for sponsoring the event!</p>
+<em
+	>Climb Talks are currently on break. If you are interested in sponsoring a future event, please
+	reach out to a moderator on our <a href="/discord">Discord server</a>.
+</em>
+
+<!-- <p>Thanks to Walmart for sponsoring the event!</p>
 
 <h2>What to expect:</h2>
 <ul>
@@ -60,9 +71,25 @@
 <p>A t-shirt and sport shorts/sweatpants are best. We will provide rental shoes free of charge.</p>
 
 <h3>Do I need to sign a waiver?</h3>
-<p>Yes, you can do it in person, or online <a href="https://bouldersandbrews.com/">here</a>.</p>
+<p>Yes, you can do it in person, or online <a href="https://bouldersandbrews.com/">here</a>.</p> -->
 
 <h2>Previous events:</h2>
+
+<h3>November 12, 2024</h3>
+<img src={nov1} width="45%" alt="Ben Burdess speaking at the November 2024 event" />
+<img src={nov2} width="45%" alt="Alexander Nelson speaking at the November 2024 event" />
+
+<div>
+	<em>
+		<strong>Ben Burdess</strong> - Designing PCBs as a Hobbyist Who Knows Nothing About Electrical Engineering
+	</em>
+	<br />
+	<em>
+		<strong>Alexander Nelson</strong> - We Have Data at Home: Decentralization of Mobile Computing
+	</em>
+</div>
+
+<h3>October 15, 2024</h3>
 <img src={oct1} width="45%" alt="Noah Zimmerman speaking at the October 2024 event" />
 <img src={oct2} width="45%" alt="Evan Black speaking at the October 2024 event" />
 
@@ -72,6 +99,7 @@
 	<em><strong>Evan Black</strong> - HTMX</em>
 </div>
 
+<h3>September 10, 2024</h3>
 <img src={sep1} width="45%" alt="Steven Judd speaking at the September 2024 event" />
 <img src={sep2} width="45%" alt="Reetik Patel speaking at the September 2024 event" />
 
@@ -81,11 +109,21 @@
 	<em><strong>Reetik Patel</strong> - Smart Home UX</em>
 </div>
 
-<style>
+<h3>April 5, 2023</h3>
+<img src={apr2} width="45%" alt="Evan Black speaking at the April 2023 event" />
+<img src={apr1} width="45%" alt="Ben Burdess speaking at the April 2023 event" />
+
+<div>
+	<em><strong>Evan Black</strong> - Due Process, an introduction to Elixir</em>
+	<br />
+	<em><strong>Ben Burdess</strong> - Battlebots: Abusing super materials for personal amusement</em>
+</div>
+
+<!-- <style>
 	.button {
 		border: 1px solid #000;
 		padding: 10px 20px;
 		border-radius: 5px;
 		text-align: center;
 	}
-</style>
+</style> -->
